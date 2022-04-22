17210d2bda50fbcec115257ef79a917f37275549
greinisch <greinisch@youwol.com>
2022-04-22 06:41:34 +0200

    🔧 yw_pipeline => expose coverage & bundle-analysis

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,6 +1,6 @@
 from youwol.environment.forward_declaration import YouwolEnvironment
 from youwol.environment.models import IPipelineFactory
-from youwol.environment.models_project import JsBundle
+from youwol.environment.models_project import JsBundle, Link
 from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
 from youwol_utils.context import Context
 
@@ -12,6 +12,9 @@ class PipelineFactory(IPipelineFactory):
 
     async def get(self, env: YouwolEnvironment, context: Context):
         config = PipelineConfig(
-            target=JsBundle()
+            target=JsBundle(links=[
+                Link(name="coverage", url="coverage/lcov-report/index.html"),
+                Link(name="bundle-analysis", url="dist/bundle-analysis.html")
+            ])
         )
         return await pipeline(config, context)
