56fad464919ba6817775c0bceb3c64e6b8014039
youwol-guillaume <greinisch@youwol.com>
2023-09-04 15:04:48 +0200

    :wrench: sync `yw_pipeline.py` & `template.py` w/ `py-youwol`

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,20 +1,20 @@
-from youwol.environment.forward_declaration import YouwolEnvironment
-from youwol.environment.models import IPipelineFactory
-from youwol.environment.models_project import JsBundle, Link
+from youwol.app.environment import IPipelineFactory, YouwolEnvironment
+from youwol.app.environment.models_project import JsBundle, Link
 from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
-from youwol_utils.context import Context
+from youwol.utils.context import Context
 
 
 class PipelineFactory(IPipelineFactory):
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     async def get(self, env: YouwolEnvironment, context: Context):
         config = PipelineConfig(
-            target=JsBundle(links=[
-                Link(name="coverage", url="coverage/lcov-report/index.html"),
-                Link(name="bundle-analysis", url="dist/bundle-analysis.html")
-            ])
+            target=JsBundle(
+                links=[
+                    Link(name="coverage", url="coverage/lcov-report/index.html"),
+                    Link(name="bundle-analysis", url="dist/bundle-analysis.html"),
+                ]
+            )
         )
         return await pipeline(config, context)
