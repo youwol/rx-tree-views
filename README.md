83326b2f5e3a3c26ef6277eb795108200233befd
youwol-guillaume <greinisch@youwol.com>
2023-09-04 15:05:34 +0200

    :wrench: update auto-generated files w/ `template.py`

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This library is part of the hybrid cloud/local ecosystem
 
 [Online user-guide](https://l.youwol.com/doc/@youwol/fv-tree)
 
-[Developers documentation](https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-tree)
+[Developers documentation](https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-tree&tab=doc)
 
 [Package on npm](https://www.npmjs.com/package/@youwol/fv-tree)
 
--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -1,11 +1,10 @@
 
 const runTimeDependencies = {
-    "load": {
+    "externals": {
         "rxjs": "^6.5.5",
         "@youwol/flux-view": "^1.0.3"
     },
-    "differed": {},
-    "includedInBundle": []
+    "includedInBundle": {}
 }
 const externals = {
     "rxjs": {
@@ -37,12 +36,27 @@ const exportedSymbols = {
         "exportedSymbol": "@youwol/flux-view"
     }
 }
+
+const mainEntry : {entryFile: string,loadDependencies:string[]} = {
+    "entryFile": "./index.ts",
+    "loadDependencies": [
+        "@youwol/flux-view",
+        "rxjs"
+    ]
+}
+
+const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {}
+
+const entries = {
+     '@youwol/fv-tree': './index.ts',
+    ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/fv-tree/${e.name}`]:e.entryFile}), {})
+}
 export const setup = {
     name:'@youwol/fv-tree',
         assetId:'QHlvdXdvbC9mdi10cmVl',
     version:'0.2.3',
     shortDescription:"Tree views using flux-view.",
-    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-tree',
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-tree&tab=doc',
     npmPackage:'https://www.npmjs.com/package/@youwol/fv-tree',
     sourceGithub:'https://github.com/youwol/fv-tree',
     userGuide:'https://l.youwol.com/doc/@youwol/fv-tree',
@@ -50,7 +64,62 @@ export const setup = {
     runTimeDependencies,
     externals,
     exportedSymbols,
+    entries,
+    secondaryEntries,
     getDependencySymbolExported: (module:string) => {
         return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    },
+
+    installMainModule: ({cdnClient, installParameters}:{
+        cdnClient:{install:(unknown) => Promise<WindowOrWorkerGlobalScope>},
+        installParameters?
+    }) => {
+        const parameters = installParameters || {}
+        const scripts = parameters.scripts || []
+        const modules = [
+            ...(parameters.modules || []),
+            ...mainEntry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/fv-tree_APIv02`]
+        })
+    },
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{
+        name: string,
+        cdnClient:{install:(unknown) => Promise<WindowOrWorkerGlobalScope>},
+        installParameters?
+    }) => {
+        const entry = secondaryEntries[name]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const parameters = installParameters || {}
+        const scripts = [
+            ...(parameters.scripts || []),
+            `@youwol/fv-tree#0.2.3~dist/@youwol/fv-tree/${entry.name}.js`
+        ]
+        const modules = [
+            ...(parameters.modules || []),
+            ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/fv-tree/${entry.name}_APIv02`]
+        })
+    },
+    getCdnDependencies(name?: string){
+        if(name && !secondaryEntries[name]){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const deps = name ? secondaryEntries[name].loadDependencies : mainEntry.loadDependencies
+
+        return deps.map( d => `${d}#${runTimeDependencies.externals[d]}`)
     }
 }
